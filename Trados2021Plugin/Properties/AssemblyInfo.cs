1540962c28b1219eb5a54baf83f7531e5118874f
niemi <nieminentommi@hotmail.com>
2021-02-05 12:13:59 +0200

    hotfix to fix valid set overlap issues, also includes changes implemented in sdl approved plugins

--- Trados2021Plugin/Properties/AssemblyInfo.cs
+++ Trados2021Plugin/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OpusCatTranslationProvider2017")]
+[assembly: AssemblyTitle("OpusCatTranslationProvider2021")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OpusCatTranslationProvider2017")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("OpusCatTranslationProvider2021")]
+[assembly: AssemblyCopyright("Copyright © 2021 Tommi Nieminen")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fc5e3080-72ab-432e-9071-fbe72f30aae6")]
+[assembly: Guid("F408E999-259C-4370-97F0-3500F9D39C30")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("21.0.0.2")]
+[assembly: AssemblyFileVersion("21.0.0.2")]
